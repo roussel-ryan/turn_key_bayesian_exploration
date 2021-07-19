cdc9e4c681ea7f41c925e4380327c07cef4248fa
Ryan Roussel <roussel@ucla.edu>
2021-07-19 15:21:39 -0500

    Simplified example script

--- acquisition/proximal.py
+++ acquisition/proximal.py
@@ -1,11 +1,12 @@
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
-from botorch.acquisition import acquisition 
+from botorch.acquisition import acquisition
 
 from botorch.utils.transforms import t_batch_mode_transform
 
+
 class ProximalAcqusitionFunction(acquisition.AcquisitionFunction):
-    def __init__(self, model, sigma_matrix, scale_to_gp = False):
+    def __init__(self, model, sigma_matrix, scale_to_gp=False):
         '''
         Acquisition function that biases other acquistion functions towards a
         nearby region in input space
@@ -20,37 +21,35 @@ class ProximalAcqusitionFunction(acquisition.AcquisitionFunction):
             the dimensionality of input space
 
         '''
-        
+
         super().__init__(model)
 
         self.register_buffer('sigma_matrix', sigma_matrix)
         self.scale_to_gp = scale_to_gp
 
-
     @t_batch_mode_transform(expected_q=1)
     def forward(self, X):
-        #get the last point in the training set (assumed to be the most
-        #recently evaluated point)
-        #print(self.model.train_inputs[0][0][-1])
-        last_pt = self.model.train_inputs[0][0][-1].double()
-        #print(self.model.train_inputs)
-        #print(last_pt)
-        #print(x)
-        #L = self.model.covar_module.outputscale
-        #dist = torch.linalg.norm((x - last_pt) / L , dim = 0)
-        
-        #define multivariate normal
-        #if self.scale_to_gp:
+        # get the last point in the training set (assumed to be the most
+        # recently evaluated point)
+        # print(self.model.train_inputs[0][0][-1])
+        last_pt = self.model.train_inputs[0][0].double()
+        # print(self.model.train_inputs)
+        # print(x)
+        # L = self.model.covar_module.outputscale
+        # dist = torch.linalg.norm((x - last_pt) / L , dim = 0)
+
+        # define multivariate normal
+        # if self.scale_to_gp:
         #    sm = torch.matmul(,self.sigma_matrix)
-        #else:
+        # else:
         sm = self.sigma_matrix
-            
+
         d = MultivariateNormal(last_pt, sm.double())
 
-        #use pdf to calculate the weighting - normalized to 1 at the last point
+        # use pdf to calculate the weighting - normalized to 1 at the last point
         norm = torch.exp(d.log_prob(last_pt).flatten())
         weight = torch.exp(d.log_prob(X).flatten()) / norm
 
-        #weight = 1 - dist
-        
+        # weight = 1 - dist
+
         return weight
